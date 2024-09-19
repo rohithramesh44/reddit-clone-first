2171df223a9e73bee7e18d92462db5ef060b2fbf
rohithramesh44 <rohithramesh44@gmail.com>
2024-09-19 14:54:26 +0530

    249 storing data to Firebase

--- lib/feature/auth/repository/auth_repository.dart
+++ lib/feature/auth/repository/auth_repository.dart
@@ -2,7 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:reddit_clone/core/constants/constant.dart';
+import 'package:reddit_clone/core/constants/firebase_constant.dart';
 import 'package:reddit_clone/core/providers/firebase_providers.dart';
+import 'package:reddit_clone/model/user_model.dart';
 
 final authRepositoryProvider = Provider((ref) => AuthRepository(
     firestore: ref.read(firestoreProvider),
@@ -17,6 +20,9 @@ class AuthRepository {
       : _firestore = firestore,
         _auth = auth,
         _googleSignIn = googleSignIn;
+
+  CollectionReference get _users =>
+      _firestore.collection(FirebaseConstants.usersCollection);
   void signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
@@ -27,6 +33,18 @@ class AuthRepository {
       );
       UserCredential userCredential =
           await _auth.signInWithCredential(credential);
+      if (userCredential.additionalUserInfo!.isNewUser) {
+        UserModel userModel = UserModel(
+          uid: userCredential.user!.uid,
+          name: userCredential.user!.displayName ?? 'No Name',
+          profilePic: userCredential.user!.photoURL ?? Constant.avatarDefault,
+          banner: Constant.bannerDefault,
+          karma: 0,
+          awards: [],
+          isAuthenticated: true,
+        );
+        await _users.doc(userCredential.user!.uid).set(userModel.toMap());
+      }
       print(userCredential.user?.email);
     } catch (e) {
       print(e);
