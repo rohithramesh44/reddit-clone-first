0137e2175424974ac6573878bd54eb35c8db52e5
rohithramesh44 <rohithramesh44@gmail.com>
2024-09-23 14:49:49 +0530

    254. home Screen App Bar Ui

--- lib/feature/home/screens/home_screen.dart
+++ lib/feature/home/screens/home_screen.dart
@@ -8,9 +8,25 @@ class HomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider)!;
     return Scaffold(
-      body: Center(
-        child: Text(user.karma.toString()),
+        appBar: AppBar(
+      title: const Text('Home'),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.search),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: CircleAvatar(
+            radius: 20,
+            backgroundImage: NetworkImage(user.profilePic),
+          ),
+        ),
+      ],
+      leading: IconButton(
+        onPressed: () {},
+        icon: const Icon(Icons.menu),
       ),
-    );
+    ));
   }
 }
